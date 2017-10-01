fff97f188fb2e1b05dba0ccc9eb07116e34653df
tejasvi-mehra <tejasvi.mehra@gmail.com>
2017-10-01 20:28:51 +0800

    here

---
+++ routes/users.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+var multer = require ('multer');
+var upload = multer({dest: './uploads'});
+var passport = require ('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+var User = require('../models/user');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register.pug', {title:'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.pug',{title:'Login'});
+});
+
+router.post('/login',
+  passport.authenticate('local',{failureRedirect: '/users/login', failureFlash: 'invalid Username/ password'}),
+  function(req, res) {
+    req.flash('sucess', 'You are now logged in')
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username,password,done){
+  User.getUserByUsername(username, function(err, user){
+    if(err) throw err;
+    if(!user){
+      return done(null,false,{message: 'Unknown User'});
+    }
+
+    User.comparePassword(password,user.password, function(err, isMatch){
+      if(err) return done(err);
+      if(isMatch) {
+        return done(null, user);
+      }else{
+        return done(null, false, {message: 'invalid Password'});
+      }
+    })
+  })
+}));
+
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	if(req.file){
+		console.log('uploading file');
+		var profileimage = req.file.filename;
+	}
+	else{
+		console.log('No file upload');
+		var profileimage = 'noimage.jpg';
+	}
+
+	//Form validator
+	req.checkBody('name', 'Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required').notEmpty();
+	req.checkBody('email', 'Email field is not email').isEmail();
+	req.checkBody('username', 'Username field is required').notEmpty();
+	req.checkBody('password', 'Password field is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+	//Check error
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register.pug', {
+			errors: errors
+		});
+	}
+	else{
+		var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });
+
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    req.flash('sucess', 'You are now registered and can login')
+
+    res.location('/');
+    res.redirect('/');
+
+	}
+
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  req.flash('sucess', 'You are now logged out');
+  res.redirect('/users/login');
+})
+
+module.exports = router;
